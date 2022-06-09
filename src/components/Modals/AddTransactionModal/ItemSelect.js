7251a44509672d36a04f8fd7a09c323d8228589d
Abdullah E CIGCI <cigcia1987@yahoo.com>
2022-06-09 17:23:27 +0400

    working on select

--- src/components/Modals/AddTransactionModal/ItemSelect.js
+++ src/components/Modals/AddTransactionModal/ItemSelect.js
@@ -3,38 +3,65 @@ import Box from "@mui/material/Box";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
+import Divider from "@mui/material/Divider";
+import FormGroup from "@mui/material/FormGroup";
+import Stack from "@mui/material/Stack";
 import Select from "@mui/material/Select";
 import { items } from "../const/itemsList.js";
 
 const ItemSelect = ({ onChange }) => {
   const [item, setItem] = useState("");
+  const [setting, setSetting] = useState("14");
 
   const handleChange = (event) => {
     setItem(event.target.value);
     onChange(event.target.value);
   };
 
+  const handleSetting = (e) => {
+    setItem("");
+    setSetting(e.target.value);
+  };
   return (
-    <Box sx={{ minWidth: 120 }}>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Diger</InputLabel>
-        <Select
-          labelId="items-select-label"
-          id="select"
-          value={item}
-          label="Urun"
-          onChange={handleChange}
-        >
-          {items
-            .filter((el) => el.view !== "button")
-            .map((item, index) => (
-              <MenuItem key={index} value={item.id}>
-                {item.label}
-              </MenuItem>
-            ))}
-        </Select>
-      </FormControl>
-    </Box>
+    <Stack
+      direction="row"
+      spacing={1}
+      justifyContent="space-between"
+      alignItems="center"
+      sx={{
+        minWidth: "150px",
+      }}
+    >
+      <Select
+        variant="standard"
+        size="small"
+        value={setting}
+        onChange={handleSetting}
+        label="Ayar"
+      >
+        <MenuItem value="14">14</MenuItem>
+        <MenuItem value="18">18</MenuItem>
+        <MenuItem value="22">22</MenuItem>
+        <MenuItem value="24">24</MenuItem>
+      </Select>
+      <Divider orientation="vertical" flexItem />
+      <Select
+        variant="standard"
+        size
+        id="select"
+        value={item}
+        label="Urun"
+        onChange={handleChange}
+      >
+        {items
+          .filter((el) => el.view !== "button" && el.setting === setting)
+          .map((item, index) => (
+            <MenuItem key={index} value={item.id}>
+              {item.label}
+            </MenuItem>
+          ))}
+      </Select>
+    </Stack>
   );
 };
 
--- src/components/Modals/AddTransactionModal/ItemsGroup.js
+++ src/components/Modals/AddTransactionModal/ItemsGroup.js
@@ -6,17 +6,16 @@ import Stack from "@mui/material/Stack";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import { items } from "../const/itemsList.js";
 import Chip from "@mui/material/Chip";
-
-import Checkbox from "@mui/material/Checkbox";
-
+import Divider from "@mui/material/Divider";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
-
 import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
 
 const ItemsGroup = () => {
   const [list, setList] = useState([]);
+  const [history, setHistory] = useState("Yeni");
+  const [type, setType] = useState("Cum");
 
   const handleSubTransactions = (id) => {
     if (!list.includes(items[id])) setList([...list, items[id]]);
@@ -28,20 +27,24 @@ const ItemsGroup = () => {
     setList(newList);
   };
 
-  const handleHistory = (index, value) => {
-    let newList = [...list];
-    newList[index].history = value;
-    setList(newList);
+  const handleHistory = (e) => {
+    setHistory(e.target.value);
+  };
+  const handleType = (e) => {
+    console.log(e.target.value);
+    setType(e.target.value);
   };
 
   const itemsButtons = items
-    .filter((el) => el.view === "button")
+    .filter(
+      (el) => el.view === "button" && el.history === history && el.type === type
+    )
     .map((item) => (
       <Button
         onClick={() => handleSubTransactions(item.id)}
         key={item.id}
         color="primary"
-        aria-label="add"
+        size="small"
       >
         {item.label}
       </Button>
@@ -57,41 +60,55 @@ const ItemsGroup = () => {
         },
       }}
     >
-      <Stack direction="row" spacing={1} alignItems="center">
-        <ButtonGroup
-          size="small"
-          variant="contained"
-          aria-label="vertical outlined button group"
-        >
-          {itemsButtons}
-        </ButtonGroup>
+      <Stack direction="column" spacing={1} alignItems="center">
+        <Stack direction="row" spacing={1} alignItems="center">
+          <FormGroup>
+            <Select
+              variant="standard"
+              size="small"
+              value={history}
+              onChange={handleHistory}
+              label="Tarih"
+            >
+              <MenuItem value="Yeni">Yeni</MenuItem>
+              <MenuItem value="Eski">Eski</MenuItem>
+            </Select>
+            <Select
+              variant="standard"
+              size="small"
+              value={type}
+              onChange={handleType}
+              label="Tarih"
+            >
+              <MenuItem value="Cum">Cum</MenuItem>
+              <MenuItem value="Ata">Ata</MenuItem>
+            </Select>
+          </FormGroup>
+          <Divider orientation="vertical" flexItem />
+          <ButtonGroup
+            size="small"
+            variant="contained"
+            aria-label="vertical outlined button group"
+          >
+            {itemsButtons}
+          </ButtonGroup>
+        </Stack>
         <ItemSelect onChange={handleSubTransactions} />
       </Stack>
+      <Divider />
       <Stack direction="column" alignItems="center" spacing={1}>
         {list.map((el, index) => (
           <Stack key={index} direction="row" alignItems="center">
             <Chip
               color="primary"
-              label={el.label}
+              label={
+                el.setting
+                  ? `${el.label}`
+                  : `${el.history} ${el.type} ${el.label}`
+              }
               variant="outlined"
               onDelete={() => handleDelete(index)}
             />
-            <FormGroup>
-              {el.history && (
-                <Select
-                  variant="standard"
-                  size="small"
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={el.history}
-                  label="Tarih"
-                  onChange={(e) => handleHistory(index, e.target.value)}
-                >
-                  <MenuItem value="Yeni">Yeni</MenuItem>
-                  <MenuItem value="Eski">Eski</MenuItem>
-                </Select>
-              )}
-            </FormGroup>
           </Stack>
         ))}
       </Stack>
