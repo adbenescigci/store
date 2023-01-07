e87ac6ea02d498d14c024d641f2a16d364b18bd6
abdenescigci <cigcia1987@yahoo.com>
2023-01-07 04:59:59 -0800

    Test Component added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
-import React, { useEffect } from "react";
-import { Outlet } from "react-router";
-import { useNavigate, useLocation } from "react-router-dom";
-import Box from "@mui/material/Box";
-import NavbarMobile from "./components/Navbar/NavbarMobile.js";
-import NavbarDesktop from "./components/Navbar/NavbarDesktop.js";
-import Header from "./components/Header/Header";
+import React, { useEffect } from 'react';
+import { Outlet } from 'react-router';
+import { useNavigate, useLocation } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import NavbarMobile from './components/Navbar/NavbarMobile.js';
+import NavbarDesktop from './components/Navbar/NavbarDesktop.js';
+import Header from './components/Header/Header';
+import Test from './Test.js';
 
 const App = () => {
   let navigate = useNavigate();
   let location = useLocation();
 
   useEffect(() => {
-    if (location?.pathname === "/") {
-      navigate("islemler");
+    if (location?.pathname === '/') {
+      navigate('islemler');
     }
   }, [location?.pathname, navigate]);
 
   const appStyles = {
     wrapper: {
-      display: "flex",
-      minHeight: "100vh",
-      flexDirection: { xs: "column", sm: "row", ms: "row" },
+      display: 'flex',
+      minHeight: '100vh',
+      flexDirection: { xs: 'column', sm: 'row', ms: 'row' },
     },
     row: {
-      width: "100%",
-      height: "100%",
-      backgroundColor: "#f5f5f5",
+      width: '100%',
+      height: '100%',
+      backgroundColor: '#f5f5f5',
     },
   };
+
+  //return <Test />;
 
   return (
     <Box sx={appStyles.wrapper}>
